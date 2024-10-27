e49cf852326bc0a0f15a1abf580f58c3db761635
Glitch (glitch-hello-website) <none>
2024-10-27 17:07:43 +0000

    👖🐝 Checkpoint ./index.html:42818678/2141 ./transitions.css:42818678/1548 ./next.html:42818678/2127 ./swup.js:42818678/284 ./base.css:42818678/4752

--- swup.js
+++ swup.js
@@ -2,6 +2,3 @@ import Swup from 'https://unpkg.com/swup@4?module';
 const swup = new Swup({
   containers: ["#swup"]
 });
-swup.hooks.on('visit:start', () => {
-  console.log(window.location.href);
-})
